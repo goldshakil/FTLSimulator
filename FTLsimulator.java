f4c05639490eb963913d1ed992c6b6afa1c15806
Muhammad Shakeel Zuhaib (Dahab) <48248272+goldshakil@users.noreply.github.com>
2020-01-10 15:51:59 +0900

    Initialization

---
+++ FTLsimulator.java
@@ -0,0 +1,24 @@
+/*
+-  Company: VLDB Lab @ Sungkyunkwan University
+
+- Programmer/Developer: Dahab Muhammad Shakeel
+
+- Main Purpose: 1) Implementing the Greedy FTL
+                2) Creating an optimal garbage collector
+
+- Details:     1) Write Unit is Page (Rgardless of Page's size)
+               2) Each Block has 4 Pages
+               3) The Simulator does OS job and sends Logical addresses
+               4) FTL translates the Logical to Physical Addresses Accordingly
+               5) FTL does the garbage collection
+               6) Addresses are read from a text file called "Addresses.txt"
+               7)
+*/
+
+class FTLsimulator
+{
+  public static void main(String[] args)
+  {
+
+  }
+}
